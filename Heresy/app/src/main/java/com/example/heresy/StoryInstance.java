1e6efa496490f540f29c766e1d7629f9ad392d32
LSJnn <729jumma@gmail.com>
2020-03-04 19:38:18 +0900

    SF 로 이어하기 구현.  - 종료 해야 저장됨.

---
+++ Heresy/app/src/main/java/com/example/heresy/StoryInstance.java
@@ -0,0 +1,284 @@
+package com.example.heresy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StoryInstance extends AppCompatActivity {
+
+    final static int STORY_BACK = 20020;
+    final static int LOAD_OK =23000;
+
+    View tInclude;View tInclude2;View tInclude3;
+    View include;View include2;
+
+    String f;String l;
+
+    ImageView h1;ImageView h2;ImageView h3;ImageView h4;
+    ImageView m1;ImageView m2;ImageView m3;ImageView m4;
+
+    TextView tv1; TextView tv2; TextView tv3;
+
+    View[] chat_1; View[] chat_2;
+
+    mySharedPreferences sf;
+    int code;
+
+    Intent intent;
+    int n = 0;
+    int new_game;
+    int recentPage;
+
+    Intent storyIntent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        onResume();
+
+        intent = getIntent();
+        f = intent.getStringExtra("firstname");
+        l = intent.getStringExtra("lastname");
+        recentPage = intent.getExtras().getInt("loadpage");
+
+        sf = new mySharedPreferences();
+
+        if(f== null && l ==null){
+            f= sf.getStringR(this, "firstname");
+            l= sf.getStringR(this, "lastname");
+        }
+
+          tv1 = findViewById(R.id.tv1);
+          tv2 = findViewById(R.id.tv2);
+          tv3 = findViewById(R.id.tv3);
+
+
+        tInclude = findViewById(R.id.include);
+        tInclude2=findViewById(R.id.include2);
+        tInclude3 = findViewById(R.id.include3);
+        include = findViewById(R.id.include_Image);
+        include2 = findViewById(R.id.include_Image2);
+
+
+        ImageView kakao_bg = findViewById(R.id.kakao_bg);
+        ImageView kakao_date = findViewById(R.id.kakao_date1);
+        h1 = findViewById(R.id.kakao_h1);
+        h2 = findViewById(R.id.kakao_h2);
+        h3 = findViewById(R.id.kakao_h3);
+        h4 = findViewById(R.id.kakao_h4);
+
+        m1 = findViewById(R.id.kakao_m1);
+        m2 = findViewById(R.id.kakao_m2);
+        m3 = findViewById(R.id.kakao_m3);
+        m4 = findViewById(R.id.kakao_m4);
+
+         chat_1 = new View[]{kakao_bg, kakao_date, h1, m1, m2, h3};
+         chat_2 = new View[]{kakao_bg, m3, h4, h4};
+
+        ImageButton next = findViewById(R.id.nextBtn);
+        ImageButton back = findViewById(R.id.backbtn);
+        //////////////////////레이아웃 객체 선언///////////////////////////////////////////////
+
+        /////////////////////카톡 화면 순서대로 구현///////////////////////////////////////////////////
+
+        System.out.println("recentPage = "+recentPage);
+        //recentPage 가 0이 아니면 = 값이 있으면 = 전에 누른 흔적이 있으면 거기부터 시작.
+
+        //new_game = intent.getExtras().getInt("NEW");
+/////////////////////////////////////////////////////////////////////////////
+        //새로하기 신호를 받아서 이어하기 할떄는 n 을 recentPage와 동기화 시키고,
+        // 새로하기 할 떄는 n 을 다시 0으로 바꿔줘야함.
+        // startActivitForReuslt 로 시도 한번....
+        //바로 위의 코드를 수정. 위에 널값 오류가 나옴...
+        ////////////////////////////////////////////////////////////////////
+
+        //스토리 진행
+/*
+        if(new_game == LOAD_OK){
+            n=0;
+        }else{
+            n = recentPage;
+        }
+*/
+        next.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                n++;
+                System.out.println("n = " + n + "\n");
+
+                sf.getPreferenences(StoryInstance.this);
+                sf.setInt(StoryInstance.this,"page",n);
+                //storyIntent.putExtra("STORY_BACK",n);
+
+                switch (n){
+                    case 1: tv1.setText("나는 4년제 대학에 다니고 있는 \n" +
+                            "평범한 대학생 "+l+f);
+                            break;
+                    case 2: include.setVisibility(View.VISIBLE);
+                            break;
+                    case 3: h1.setVisibility(View.VISIBLE);
+                            break;
+                    case 4: m1.setVisibility(View.VISIBLE);
+                            break;
+                    case 5: m2.setVisibility(View.VISIBLE);
+                        break;
+                    case 6: h2.setVisibility(View.VISIBLE);
+                        break;
+                    case 7: h3.setVisibility(View.VISIBLE);
+                             break;
+                    case 8: tv2.setText("혜준이는 \n 초등학생때부터 쭉 친구다");
+                            break;
+                    case 9: tInclude.setVisibility(View.GONE);
+                            include.setVisibility(View.GONE);
+                            include2.setVisibility(View.VISIBLE);
+                            break;
+                    case 10:m3.setVisibility(View.VISIBLE);
+                            break;
+                    case 11:h4.setVisibility(View.VISIBLE);
+                            break;
+                    case 12:m4.setVisibility(View.VISIBLE);
+                            break;
+                    case 13: tInclude3.setVisibility(View.VISIBLE);
+                            tv3.setText("답답한 일상에 상담이라도 받아보면\n" +
+                            " 나아질까 하는 마음이다\n" +
+                            "\n" +
+                            "…\n");
+
+
+                }
+
+            }
+        });
+
+        if(recentPage >= 2){
+            n = recentPage -2;
+        }else if(recentPage == 1){
+            n = recentPage -1;
+        }else if(recentPage==0){
+            n = recentPage;
+        }
+
+
+        // sf 에 들어있는 page 키에 아무것도 안 들어 있으면 0 을 반환.//새로 시작.
+
+        //////////////////이전 버튼 눌렀을 떄///////////////////////////////////////////////
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+             //storyIntent = new Intent(Story.this, MainActivity.class);
+             //storyIntent.putExtra("STORY_BACK",n);//현재 페이지
+             setResult(STORY_BACK);
+             finish();
+            }
+        });
+
+/*        if (sum == 1) {
+            tv1.setText("나는 4년제 대학에 다니고 있는 \n" +
+                    "평범한 대학생 " + i.getExtras() + l + f + "\n");
+        }
+        chat_1[0].setVisibility(View.VISIBLE);
+        chat_1[1].setVisibility(View.VISIBLE);
+    */
+/* while(sum <=5 ) {
+     chat_1[sum].setVisibility(View.VISIBLE);*/
+        /*
+
+         */
+/*            if (sum >= 7) {
+                for(int k=1;k<chat_1.length;k++){
+                    chat_1[k].setVisibility(View.INVISIBLE);
+                }
+                for (int t = 5; sum<12; t++) {
+                    chat_2[t-5].setVisibility(View.VISIBLE);
+                }
+            }*//*
+
+            }else if(sum<0){
+            finish();
+*/
+
+/////////////////////////////back/ next 버튼 /////////////////////
+
+
+/////////////////////배열에 1글자씩 저장///////////////////////.//
+/*        String text = tv1.getText().toString();
+        String[] eachString;
+
+        Typing typing = new Typing(text);
+        typing.string_split();
+
+
+        eachString = typing.array_story;*/
+
+//////////////////한 글자씩 출력////////////////--> 실패... timer 함수는 run 안의 내용을 일정 간격마다 반복하는 것. 여기서 for 문을 사용하든 뭘하든 안됨.
+
+        //Read(eachString,tv2);
+
+/*
+        String saying = "";
+        for(int a=0;a<eachString.length;a++){
+
+            String s = eachString[a];// 1 2/
+            saying +=s; //1 +2 +3
+//            String s = eachString[a];
+
+            System.out.println(saying);
+            tv2.setText(saying);
+        }
+*/
+
+    }
+
+    ///////////////////////////SharedPreferences/////
+/*    @Override
+    protected void onStop() {
+        super.onStop();
+        // activity 종료 전에 loadPage 파일에 저장.
+        SharedPreferences sharedPreferences  = getSharedPreferences("loadPage",MODE_PRIVATE);
+        //저장하기 위해 editor 사용
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        int memory = n;
+        editor.putInt("page",memory);
+        editor.commit();
+    }*/
+
+    public void Do(int i, View[] arr){
+        for(; i<arr.length;){
+            Toast.makeText(this, n+"입니다.",Toast.LENGTH_LONG).show();
+        }
+    }
+    public void Read(String[] arr, TextView tv){
+
+        String saying ="";
+        ReadingStory readingStory = new ReadingStory();
+
+        for(int a=0;a<arr.length;a++){
+
+            String s = arr[a];// 1 2/
+            saying +=s; //1 +2 +3
+
+            System.out.println(saying);
+            readingStory.setSettings(saying,tv,arr);
+            readingStory.readingTimerTask();
+
+        }readingStory.stopTimerTask();
+
+
+
+    }
+
+
+}
+
